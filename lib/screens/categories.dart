a11f809efd305663798a9207e4bfbfd97b3e82ce
Tamby Tia RAZAFINDRALAMBO <tambytia.razafindralambo@elca.mu>
2025-05-28 10:58:31 +0400

    explicit animation

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -5,41 +5,77 @@ import 'package:demo4/screens/meals.dart';
 import 'package:demo4/widgets/category_grid_item.dart';
 import 'package:flutter/material.dart';
 
-class CategoriesScreen extends StatelessWidget {
+class CategoriesScreen extends StatefulWidget {
   final List<Meal> meals;
 
   const CategoriesScreen({super.key, required this.meals});
 
   @override
+  State<CategoriesScreen> createState() => _CategoriesScreenState();
+}
+
+class _CategoriesScreenState extends State<CategoriesScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 300),
+      // lowerBound: 0,
+      // upperBound: 1
+    );
+    _animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: GridView(
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          childAspectRatio: 3 / 2,
-          crossAxisSpacing: 20,
-          mainAxisSpacing: 20,
+    return AnimatedBuilder(
+      animation: _animationController,
+      child: Scaffold(
+        body: GridView(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            childAspectRatio: 3 / 2,
+            crossAxisSpacing: 20,
+            mainAxisSpacing: 20,
+          ),
+          children: [
+            for (final cat in availableCategories)
+              CategoryGridItem(
+                category: cat,
+                onSelectCategory: () {
+                  _selectCategory(context, cat);
+                },
+              ),
+          ],
         ),
-        children: [
-          for (final cat in availableCategories)
-            CategoryGridItem(
-              category: cat,
-              onSelectCategory: () {
-                _selectCategory(context, cat);
-              },
-            ),
-        ],
       ),
+      builder:
+          (context, child) =>
+              Padding(padding: EdgeInsets.only(top: 100 - _animationController.value * 100), child: child),
     );
   }
 
   void _selectCategory(BuildContext context, Category category) {
-    final filteredMeals = meals.where((meal) => meal.categories.contains(category.id)).toList();
+    final filteredMeals =
+        widget.meals
+            .where((meal) => meal.categories.contains(category.id))
+            .toList();
 
     Navigator.push(
       context,
       MaterialPageRoute(
-        builder: (ctx) => MealsScreen(title: category.title, meals: filteredMeals),
+        builder:
+            (ctx) => MealsScreen(title: category.title, meals: filteredMeals),
       ),
     );
   }
